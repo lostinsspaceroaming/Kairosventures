b1bb6c87d22463026d8d80886d57a35094c6bddb
lostinsspaceroaming <caccogan@outlook.com>
2025-04-18 21:36:17 +0100

    Fix deployment configuration and dependencies

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
       },
     ],
+  },
+  experimental: {
+    serverActions: true,
   },
 }
 
