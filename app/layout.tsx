b866a50b1f47a0d4de8672923293f26a31143b66
lostinsspaceroaming <caccogan@outlook.com>
2025-04-18 21:54:01 +0100

    Trigger new deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        {/* Force new deployment */}
         <motion.header 
           initial={{ y: -100 }}
           animate={{ y: 0 }}
